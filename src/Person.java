d5158b8728bf504a81583423253b6abd6420a62f
RobloxYesCaillouNo <league@WTS13.attlocal.net>
2019-02-24 17:22:11 -0800

    I started Getting and Setting (Taco and Person)

---
+++ src/Person.java
@@ -0,0 +1,12 @@
+
+public class Person {
+	//Part 2:
+		//4. Create a Person class, with 2 member variables - name and superpower.
+
+		//5. Using the pattern in Part 1, add getter and setter methods for the member variables of the Person class.
+
+		//6. Now, make a Runner class that creates 3 different people, sets their superpowers, and prints the value returned by the default toString() method.
+
+		//7. Override the default toString() method by implementing a toString() method 
+		//on the Person class that returns "<name> has mad <superpower> skills". 
+}
---
+++ src/Taco.java
@@ -0,0 +1,12 @@
+
+public class Taco {
+	//Part 1:
+	//1: Create a class called Taco, then add 2 private String member variables for the meat and sauce.
+
+	//2: Create methods getMeat() and getSauce() that return the member variables.
+
+	//3: Create methods setMeat(String meat) and setSauce(String sauce) 
+	//that set the values of the members variables to the values passed in the method parameters. 
+
+
+}
